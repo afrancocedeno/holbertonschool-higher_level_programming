6bb0862d5baa93fe318050583d62eb19e0b1e1e3
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-10 21:18:10 +0000

    01 | code fix | code order and if syntax

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+if __name__ != "__main__":
+    exit()
 import calculator_1 as calc
-if __name__ == "__main__":
-    a = 10
-    b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
+a = 10
+b = 5
+print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, calc.sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, calc.mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, calc.div(a, b)))
