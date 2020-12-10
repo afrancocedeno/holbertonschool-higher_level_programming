f1e7831aee80964418d9bf55825c993a2303c2fb
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-10 21:40:09 +0000

    01 | file update | no difference in this case from single or double cuotes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-print('Number of arguments: {}'.format(len(sys.argv)))
+print("{} Number of arguments: ".format(len(sys.argv)))
 print('Argument(s) passed: {}'.format(str(sys.argv)))
