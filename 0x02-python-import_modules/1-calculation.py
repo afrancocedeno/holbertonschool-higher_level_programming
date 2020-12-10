1aa26f8c0a87cfcdd25d39ca0f9c68c22a89121d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-10 21:28:38 +0000

    02 | code fix | iport level line

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+import calculator_1 as calc
 if __name__ != "__main__":
     exit()
-import calculator_1 as calc
 a = 10
 b = 5
 print("{:d} + {:d} = {:d}".format(a, b, calc.add(a, b)))
