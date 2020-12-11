39efae924ca76d72681c6a85c585a617c0ee734e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-11 02:42:44 +0000

    02 | code try | output done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-import sys
-
-print("{} Number of arguments: ".format(len(sys.argv)))
-print('Argument(s) passed: {}'.format(str(sys.argv)))
+if __name__ == "__main__":
+	from sys import argv
+	l = len(argv)
+	if l == 1:  # somthing that saves the number of arguments
+		print("{:d} arguments.".format(l - 1))
+	elif l == 2:  # somthing that saves the number of arguments
+		print("1 argument:\n1: {}".format(argv[1]))
+	else:
+		print("{:d} arguments: ".format(l - 1))
+		arg_number = 1
+		for i in argv:  # from the first argument till the last
+			if i != argv[0]:  # not valid != 0
+				print("{}: {}".format(arg_number, i))
+				arg_number += 1
