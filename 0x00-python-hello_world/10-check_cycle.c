50b27de34bb491cdf425f9e08921aeb3187abf0d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-11 21:24:44 +0000

    03 | code fix | floyd cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,7 +6,7 @@
 int check_cycle(listint_t *list)
 {
 	int i = 0;
-	listint_t *head = list;
+	listint_t *head = list, *speed_pointer = NULL;
 
 	if (!list)
 		return (0);
@@ -15,6 +15,9 @@ int check_cycle(listint_t *list)
 		if ((*list).next == head)
 			return (1);
 		list = (*list).next;
+		speed_pointer = (*(*speed_pointer).next).next; /* equals to speed_pointer->next->next */
+		if (speed_pointer != head)
+			return (1);
 	}
 	return (0);
 }
