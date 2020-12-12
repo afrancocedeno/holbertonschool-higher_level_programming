5f8f03b9be6124d1d1c41a60abab8168ecfd6465
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-12 01:51:34 +0000

    04 | code fix | commit back to 02

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,18 +6,15 @@
 int check_cycle(listint_t *list)
 {
 	int i = 0;
-	listint_t *head = list, *speed_pointer = NULL;
+	listint_t *head = list;
 
-	if (!list)
+	if (list != NULL)
 		return (0);
 	for (; (*list).next; i++) /* is equal to (*list).next != NULL */
 	{
 		if ((*list).next == head)
 			return (1);
 		list = (*list).next;
-		speed_pointer = (*(*speed_pointer).next).next; /* equals to speed_pointer->next->next */
-		if (speed_pointer != head)
-			return (1);
 	}
 	return (0);
 }
