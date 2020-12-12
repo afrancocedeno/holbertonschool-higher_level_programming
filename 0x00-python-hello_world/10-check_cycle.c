b5991eb948f53fc46da9cc75b6d93d045682025b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-12 03:18:47 +0000

    08 | code try | output done try

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,20 +5,19 @@
  */
 int check_cycle(listint_t *list)
 {
-	int i = 0;
 	listint_t *head = list, *speed_pointer = NULL;
 
 	speed_pointer = list;
 	if (!list) /* == NULL equals from unexistance */
 		return (0);
-	for (; speed_pointer != list; i++)
+	while (list && speed_pointer) /* while they exist */
 	{
 		list = (*list).next;
 		speed_pointer = (*(*speed_pointer).next).next;
 		if ((*list).next == head)
-			return (0);
+			return (1);
 		if (speed_pointer == list)
 			return (1);
 	}
-	return (1);
+	return (0);
 }
