4269c0938fc82e94b6257e21b6d30d532508f6a2
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-12 02:07:00 +0000

    06 | code try | accesing syntax try

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -6,15 +6,18 @@
 int check_cycle(listint_t *list)
 {
 	int i = 0;
-	listint_t *head = list;
+	listint_t *head = list, *speed_pointer = NULL;
 
 	if (!list) /* == NULL equals from unexistance */
 		return (0);
 	for (; (*list).next; i++) /* is equal to (*list).next != NULL */
 	{
+		if ((*speed_pointer).next == list)
+			return (1);
 		if ((*list).next == head)
 			return (1);
 		list = (*list).next;
+		speed_pointer = (*(*speed_pointer).next).next;
 	}
 	return (0);
 }
