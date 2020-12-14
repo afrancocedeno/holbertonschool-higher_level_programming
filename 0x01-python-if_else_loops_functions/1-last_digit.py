65cab22745140e4b6e8ce3bd9e7b04318f82f8d6
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 05:30:28 +0000

    01 | file create | pep8 line long

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-1000, 10000)
-# print("Last digit of", number, "is\n", number % 10)
-print("Last digit of {:d} is {:d}".format(number, number % 10 if number > 0 else number % -10))
+n = random.randint(-1000, 10000)
+# print("Last digit of", n, "is\n", n % 10)
+print("Last digit of {:d} is {:d}".format(n, n % 10 if n > 0 else n % -10))
