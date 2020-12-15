c58bf1940f73005e4dcc9cb7ee87e77f1331c019
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 12:27:31 +0000

    00 | file create | output done without str.format

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# from main functions we must use prototype in funciton file
+def print_list_integer(my_list=[]):
+# don´t forget ":" in statements
+    for i in my_list:
+        print(i)
