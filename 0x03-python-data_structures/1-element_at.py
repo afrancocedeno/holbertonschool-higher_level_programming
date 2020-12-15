c65de472b294a724178de034c43852bf42ae45dc
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 13:20:00 +0000

    05 | code try | output done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
 # return as a full statement
-  return None if idx < 0 or idx >= len(my_list) else idx
+    return None if idx < 0 or idx >= len(my_list) else my_list[idx]
