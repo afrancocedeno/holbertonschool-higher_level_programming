7b663c0933758d0a9ca82d54bc80d40ab084b836
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 18:33:25 +0000

    01 | code try | output almos done error list out of range

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pymatrixhon3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(j, end=" ")
-        print()
+        for j in i[:-1]:
+            print("{:d}".format(j), end=" ")
+        print("{:d}".format(i[-1]))
