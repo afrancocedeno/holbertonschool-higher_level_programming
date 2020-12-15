062c77c5014acec2ab6bdcdb6738c5330de5bf10
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 13:15:22 +0000

    02 | code try | for loop tried always return nonepython3 1-main.py !

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    l = len(my_list)
+    print(l)
     for i in my_list:
-        if i == idx:
-            my_list
+        if idx < 0 or idx >= len(my_list):
+            return None
+        else:
+            my_list[idx]
