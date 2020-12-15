340ef0dc20af67303b03d41785032659a4c3c251
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 18:08:56 +0000

    00 | file create | output done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/pymatrixhon3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print(matrix)
+        for j in i:
+            print(j, end=" ")
+        print()
