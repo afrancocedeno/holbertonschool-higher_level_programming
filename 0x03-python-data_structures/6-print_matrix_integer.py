89f638dada6f020ee1deaa23ac2f64307ef9610a
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-15 19:20:53 +0000

    06 | code try | line optimized

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:  # matrix != None
-        for i in matrix:
-            for j in i[:-1]:
-                print("{:d}".format(j), end=" ")
-            if i:
-                print("{:d}".format(i[-1]))
-            else:
-                print()
+    for i in matrix:
+        for j in i[:-1]:
+            print("{:d}".format(j), end=" ")
+        if i:
+            print("{:d}".format(i[-1]))
+        else:
+            print()
