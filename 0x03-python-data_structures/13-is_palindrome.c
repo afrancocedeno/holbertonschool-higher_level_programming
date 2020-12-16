4a47d5d24aeb25b1d9469e0e847100296996d6c4
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-16 04:47:56 +0000

    02 | code try | loop increment fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -16,8 +16,11 @@ int is_palindrome(listint_t **head)
 	/* buffering */
 	for (; i < 20; i++)
 		buffer[i] = '\0';
-	for (i = 0; aux_ptr; i++, aux_ptr = (*aux_ptr).next)
+	for (i = 0; aux_ptr; i++)
+	{
 		buffer[i] = (*aux_ptr).n;
+		aux_ptr = (*aux_ptr).next;
+	}
 	for (; j <= i; j++, i--)
 		if (buffer[j] != buffer[(i - 1)])
 			return (0);
