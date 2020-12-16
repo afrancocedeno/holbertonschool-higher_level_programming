e68b049de1c4ddad82ca5c7e3827c77a169983b1
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-16 04:56:28 +0000

    06 | code try | accesing head.next fix

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,7 +13,7 @@ int is_palindrome(listint_t **head)
 	listint_t *aux_ptr = NULL;
 
 	aux_ptr = *head;
-	if (!*head || !(*head).next)
+	if (!*head || !(*(*head)).next)
 		return (1);
 	/* buffering */
 	for (; i < 20; i++)
