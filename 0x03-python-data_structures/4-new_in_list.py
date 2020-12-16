32b6aa673fce71033bbc516eedcc06a575581756
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-16 00:40:37 +0000

    01 | code try | output done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,5 +4,4 @@ def new_in_list(my_list, idx, element):
     if idx > 0 or idx <= len(new_list):
         new_list[idx] = element
         return new_list
-    else:
-        return None
+    return my_list
