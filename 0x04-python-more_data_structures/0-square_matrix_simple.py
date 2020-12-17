6e9173434c223439af475b6f126ce118469d0f0e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-17 10:03:22 +0000

    01 | code try | output done using nested loops

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
+def square_matrix_simple(matrix=[], new_matrix=[]):
+    new_matrix = [row[:] for row in matrix]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] = matrix[i][j] * matrix[i][j]
     return new_matrix
