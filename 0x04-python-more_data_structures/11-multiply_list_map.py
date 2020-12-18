884a6dfb0be0f10b9f227b1c422d3ba6a63f05f5
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-18 03:52:43 +0000

    00 | code try | error output -<map object at 0x7fe1be7d2cf8>-

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list = my_list
-    for i in range(len(my_list)):
-        new_list[i] = my_list[i] * number
-    return new_list
+    return map(my_list * number, my_list)
