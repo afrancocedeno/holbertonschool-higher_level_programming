6440eae9ab32cbe3ec2757b5bd7788d2cb734ea8
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-04 18:47:27 +0000

    02 | code try | output done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,17 @@
 
 
 # how do I receive arguments froms another funciton in python?
-def safe_print_list(my_list=[], items_qty=0):
-    for i in range(items_qty):
+def safe_print_list(my_list=[], items_request=0):
+    items_give = 0
+    for i in range(items_request):
         try:
             # end="" doesn´t print a new line
             print("{}".format(my_list[i]), end="")
+        # not specifying the exception will consider all expressions
         except:
             # stop the loop
             break
+        items_give += 1
     # print a new line at the end of the loop
     print()
-    return items_qty
+    return items_give
