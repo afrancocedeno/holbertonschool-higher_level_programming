9827430033c3a241e377548f29cf14e235234e49
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-06 04:45:49 +0000

    01 | code try | position method fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -27,12 +27,12 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if ((isinstance(value, tuple)) and len(value) == 2 and
-                (value[0] >= 0 and value[1] >= 0) and
-                (isinstance(value[0], int)) and (isinstance(value[1], int))):
-            self.__position = value
-        else:
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or value[0] < 0 or \
+           type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     """funciton that calculates the area of an square"""
     def area(self):
