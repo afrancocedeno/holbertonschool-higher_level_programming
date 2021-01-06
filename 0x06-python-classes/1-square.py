edc13d2224787f97138373e9e6f1d58375aad4ec
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-06 04:00:43 +0000

    00 | file create | output done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class that defines a square by:
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+"""
+
+
+class Square:
+    """class body"""
+    def __init__(self, size):
+        self.__size = size
