528a3150d060235cc3d2c9747578a44b80ec1c78
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-19 21:32:45 +0000

    00 | file create | output done

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,7 +9,11 @@ def lookup(obj):
     """function that get into objet attributes.
 
     This function use dir to extract all attributes
-    if an object
+    if an object.
+
+    dir() funciton is a builtin that returns a list of
+    the attributes and methods of any object ( say funcitons,
+    modules, strings, lists, dictionaries, etc.)
 
     Args:
         obj: input object to be extracted
@@ -18,5 +22,5 @@ def lookup(obj):
         Returns a list of attrbutes
 
     """
-    attr_list = dir(obj)
+    attr_list = dir(obj1)
     return (attr_list)
