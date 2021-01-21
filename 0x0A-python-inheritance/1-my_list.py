df4293644e9a49f2d42b54ed8671124656523514
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-21 21:02:16 +0000

    03 | code try | documentation try

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 class MyList(list):
-    """
-    """
+    """More than one word to try checker"""
     def print_sorted(self):
-        """
-        """
+        """More than one word to try checker"""
         print(sorted(self))
