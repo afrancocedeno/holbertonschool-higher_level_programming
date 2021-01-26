215f0ba762a183cd0b4b04fafd52da8bd2291d34
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-26 18:46:40 -0500

    01 | code fix | output logic with conditional behind

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     counter = 0
     new_str = ""
     for i in str:
-        counter += 1
         if counter != n:
             new_str += i
+        counter += 1
     return(new_str)
