69fbb48b841d0220d71965304d7543d5a358e674
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-01-26 15:47:07 -0500

    01 | code fix | format specifier

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,7 +6,7 @@
 def uppercase(str):
     for i in list(str):
         if 97 <= ord(i) <= 122:
-            print(chr(ord(i) - 32), end="")
+            print("{:s}".format(chr(ord(i) - 32)), end="")
         else:
-            print(i, end="")
+            print("{:s}".format(i), end="")
     print()
