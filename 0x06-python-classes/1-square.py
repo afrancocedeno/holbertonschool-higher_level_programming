988132bc99aa01823d01fffb22d1d55ae035640d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-08 00:17:29 -0500

    01 | file update | understanding docstrings in modules class and funct

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,29 @@
-#!/usr/bin/python3
-"""class that defines a square by:
-Private instance attribute: size
-Instantiation with size (no type/value verification)
-"""
+"""This module demostrates and practices documentation in .py files"""
 
 
 class Square:
-    """class body"""
-    def __init__(self, size):
+    """This class defines a square object"""
+
+    def __init__(self, size="0"):
+        """
+        __init__ is a special method that initializes everything inside
+        it when an object is created with its class.
+
+        print(size) is different from print(self.size)
+
+        Args:
+            size (int): Size of the square. (This is the appropiate
+                docstring method)
+
+            (self is an arguments itself, so it shouldn´t be included
+            in this section but in this function I´m using 2 values:
+            self: Is used to send the object itself to the function.
+            size: the second parameter with the size of the square.)
+
+        Returns:
+            Type: can be a bool, int, string, among others.
+
+            Bool: True if successful, False otherwise.
+
+        """
         self.__size = size
