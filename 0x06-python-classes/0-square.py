20982619fbc8a8b8a37b5ca03106701b25bc2f35
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-07 23:22:10 -0500

    03 | code try | docstrings class different from the header section

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Empty class that defines a square.
+'''Section header docstring.
 
 __dict__: acces the class method docstring.
 object_name.__dict__                --> acces the docstring of the class
@@ -9,5 +9,6 @@ object_name.function_name.__dict__  --> acces the docstring of the class
 
 
 class Square:
+    '''Empty class that defines a square.'''
     def function(self):
         pass
