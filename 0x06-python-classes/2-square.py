1dd54818f001b781b273c9aee07b1c2f1e624d79
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-02-09 12:32:13 -0500

    01 | code try | diferent solutions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-"""square class"""
+"""module"""
 
 
 class Square:
-    """class body, exception handling"""
+    """class that defines a square with exception handling."""
     def __init__(self, size=0):
-        if type(size) is not int:
+        """function
+        if statemante variation:
+            if (type(size) is not):
+            if (not isinstance(size, int)):
+            if (isinstance(size, int) != True):
+        """
+        if (isinstance(size, int) is not True):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
