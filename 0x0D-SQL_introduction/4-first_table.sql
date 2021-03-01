02f899c76c9b011a029ae43f81e79914c942fe5e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-01 17:47:07 -0500

    01 | file update | comment added

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
 -- creates a table called first_table in the current database
---
+-- create table if already exist not fail.
 CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256));
