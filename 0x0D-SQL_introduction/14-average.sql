ae06a7faeb42d7f0384eb9f4102be68565353622
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-01 23:21:16 -0500

    00 | file create | output done

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,8 @@
+-- script that computes the score average of all records
+-- in the table second_table of the database hbtn_0c_0
+-- https://www.w3schools.com/sql/sql_count_avg_sum.asp
+-- SELECT AVG(column_name)
+-- FROM table_name
+-- WHERE condition;
+SELECT AVG(score)
+FROM second_table
