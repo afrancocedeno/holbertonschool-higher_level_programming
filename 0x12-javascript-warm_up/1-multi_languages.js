672f330521237b7059d757b15f4679557efa1bf7
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 17:07:43 -0500

    00 | file create | output done using multi-line string

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+using a multi-line string:
+https://gist.github.com/jeromeetienne/6257420
+*/
+const myVar = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+].join('\n');
+console.log(myVar);
