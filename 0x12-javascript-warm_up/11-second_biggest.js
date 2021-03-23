5849e2735a079ac5b181278fdd7164450b6cf869
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:45:26 -0500

    11 | code try | semistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 let i = 0;
+
+// sort in reverse
 myArgs.sort().reverse();
+
+// Edge cases
 if (process.argv.length <= 3) console.log(0);
+
+// if more than one argument
 else {
   for (; i < process.argv.length; i++) {
-    if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; } else if (myArgs[i + 1] < myArgs[i]) {
+    if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; }
+    if (myArgs[i + 1] < myArgs[i]) {
       console.log(myArgs[i + 1]);
       break;
     }
