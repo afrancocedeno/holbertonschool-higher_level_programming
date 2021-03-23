b1d1ffd2f22e22fb98e46f15a08de01e7221c265
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-23 00:08:55 -0500

    01 | code try | semistandard

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function callMeMoby (x, theFunction) {
-	let i = 0;
-	for (; i < x; i++) { theFunction(); };
-      }
+  let i = 0;
+  for (; i < x; i++) { theFunction(); }
+}
 module.exports.callMeMoby = callMeMoby;
