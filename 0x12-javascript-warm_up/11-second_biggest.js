b1741bfcf936a964db8ced6baea33221285589df
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:22:20 -0500

    06 | code try | first try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,13 @@
-#!/usr/bin/node
+#!/usr/bin / node
 const myArgs = process.argv.slice(2);
 let i = 0;
-// sort in reverse
 myArgs.sort().reverse();
-// edge cases
 if (process.argv.length === 2 || process.argv.length === 3) console.log(0);
 else {
-  for (; i < process.argv.length; i++) {
-    if (myArgs[i] !== myArgs[i - 1]) {
-      console.log(myArgs[i]);
-      break;
-    }
-  }
+	for (; i < process.argv.length; i++) {
+		if (myArgs[i + 1] < myArgs[i]) {
+			console.log(myArgs[i + 1]);
+			break;
+		}
+	}
 }
