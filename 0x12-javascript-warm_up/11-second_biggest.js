6927585de900bca9dfcc1200f403ceb80e2810b7
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:11:05 -0500

    01 | code try != comparator

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,8 @@ let i = 0;
 myArgs.sort().reverse();
 if (process.argv.length === 2 || process.argv.length === 3) console.log(0);
 for (; i < process.argv.length; i++) {
-  if (myArgs[i + 1] < myArgs[i]) {
-    console.log(myArgs[i + 1]);
+  if (myArgs[i] != myArgs[i - 1]) {
+    console.log(myArgs[i]);
     break;
   }
 }
