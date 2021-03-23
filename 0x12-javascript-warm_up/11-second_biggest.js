335e325b1774ca0dca1772ba1a29aeeb57587cb0
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:59:46 -0500

    no reverse method used

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const myArgs = process.argv.slice(2);
 let i = 0;
 
 // sort in reverse
-myArgs.sort().reverse();
+myArgs.sort(function(a, b) { return (b - a); });
 
 // Edge cases
 if (process.argv.length <= 3) console.log(0);
@@ -11,6 +11,7 @@ if (process.argv.length <= 3) console.log(0);
 // if more than one argument
 else {
   for (; i < process.argv.length; i++) {
+    // if end of the list
     if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; }
     if (myArgs[i + 1] < myArgs[i]) {
       console.log(myArgs[i + 1]);
