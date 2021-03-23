5cbbae226b1d94f9a7256cbe12a60052f3bfe3fb
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:40:35 -0500

    10 | code try | semistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,8 +5,7 @@ myArgs.sort().reverse();
 if (process.argv.length <= 3) console.log(0);
 else {
   for (; i < process.argv.length; i++) {
-if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; }
-else if (myArgs[i + 1] < myArgs[i]) {
+    if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; } else if (myArgs[i + 1] < myArgs[i]) {
       console.log(myArgs[i + 1]);
       break;
     }
