7ad85d1d8a8992f653e858b2b7a1b1ac3d2ceff3
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:29:55 -0500

    08 | code try | output done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,12 +2,13 @@
 const myArgs = process.argv.slice(2);
 let i = 0;
 myArgs.sort().reverse();
-if (process.argv.length === 2 || process.argv.length === 3) console.log(0);
+if (process.argv.length <= 3) console.log(0);
+else if (myArgs[i + 1] === myArgs[i]) console.log(myArgs[i]);
 else {
-	for (; i < process.argv.length; i++) {
-		if (myArgs[i + 1] < myArgs[i]) {
-			console.log(myArgs[i + 1]);
-			break;
-		}
-	}
+  for (; i < process.argv.length; i++) {
+    if (myArgs[i + 1] < myArgs[i]) {
+      console.log(myArgs[i + 1]);
+      break;
+    }
+  }
 }
