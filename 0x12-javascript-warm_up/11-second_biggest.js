082c9515ddc1a60b9dd75de21c078666272abc39
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-22 23:36:53 -0500

    09 | code try | if equal break

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@ const myArgs = process.argv.slice(2);
 let i = 0;
 myArgs.sort().reverse();
 if (process.argv.length <= 3) console.log(0);
-else if (myArgs[i + 1] === myArgs[i]) console.log(myArgs[i]);
 else {
   for (; i < process.argv.length; i++) {
-    if (myArgs[i + 1] < myArgs[i]) {
+if (isNaN(myArgs[i + 1])) { console.log(myArgs[i + 1]); break; }
+else if (myArgs[i + 1] < myArgs[i]) {
       console.log(myArgs[i + 1]);
       break;
     }
