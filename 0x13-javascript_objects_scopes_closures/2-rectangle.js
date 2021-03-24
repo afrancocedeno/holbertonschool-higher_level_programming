a78b04f6e9d64d9d62a0abd951775e3fc5a06cdd
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-23 22:35:49 -0500

    01 | code try | semistandard check

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,11 @@
 class Rectangle {
   // How to define the class declarations
   constructor (w, h) {
-	  if (w > 0 && h > 0) {
-	    this.width = w;
-	    this.height = h;
-	  }
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
+
 module.exports = Rectangle;
