4d5714af2ce4f68463cbd4dc4661cb2dd6d61012
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-23 21:08:30 -0500

    02 | code try | semistandard check

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,9 +3,8 @@ const copySquare = require('./5-square');
 // use a differente name inheritance
 module.exports = class Square extends copySquare {
   // create the instance method
-  charPrint(c) {
+  charPrint (c) {
     // if c is undefined use char X
-    if (c === undefined) { this.print(); }
-    else { for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }}
+    if (c === undefined) { this.print(); } else { for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); } }
   }
-}
+};
