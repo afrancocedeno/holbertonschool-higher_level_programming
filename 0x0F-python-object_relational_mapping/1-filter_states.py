030377530e09125bcb0a37f372a1e06bc8de26ef
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-27 18:56:49 -0500

    01 | code try | is statement added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,26 +3,27 @@
 script that lists all states with a name starting
 with N (upper N) from the database hbtn_0e_0_usa
 """
+if __name__ == "__main__":
 
-import MySQLdb
-import sys
+    import MySQLdb
+    import sys
 
-credentials = {
-    'host': "localhost",
-    'port': 3306,
-    'user': sys.argv[1],
-    'passwd': sys.argv[2],
-    'db': sys.argv[3],
-    'charset': "utf8"
-}
+    credentials = {
+        'host': "localhost",
+        'port': 3306,
+        'user': sys.argv[1],
+        'passwd': sys.argv[2],
+        'db': sys.argv[3],
+        'charset': "utf8"
+    }
 
-query = 'SELECT * FROM states WHERE name REGEXP \'^[N]\' ORDER BY id ASC;'
+    query = 'SELECT * FROM states WHERE name REGEXP \'^[N]\' ORDER BY id ASC;'
 
-connection = MySQLdb.connect(**credentials)
+    connection = MySQLdb.connect(**credentials)
 
-with connection.cursor() as cursor:
-    cursor.execute(query)
-    data_rows = cursor.fetchall()
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        data_rows = cursor.fetchall()
 
-connection.close()
-print(*data_rows, sep='\n') if (data_rows) else 0
+    connection.close()
+    print(*data_rows, sep='\n') if (data_rows) else 0
