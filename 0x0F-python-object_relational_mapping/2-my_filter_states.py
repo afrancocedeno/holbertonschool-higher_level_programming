f2516bf8a387e5143eb82758da402ffe84c028c6
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-27 20:26:57 -0500

    02 | code try | avoid execution when imported

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
+def main():
+    import MySQLdb
+    import sys
 
-credentials = {
-    'host': "localhost",
-    'port': 3306,
-    'user': sys.argv[1],
-    'passwd': sys.argv[2],
-    'db': sys.argv[3],
-    'charset': "utf8"
-}
+    credentials = {
+        'host': "localhost",
+        'port': 3306,
+        'user': sys.argv[1],
+        'passwd': sys.argv[2],
+        'db': sys.argv[3],
+        'charset': "utf8"
+    }
 
-query = '''
-        SELECT * FROM states
-        WHERE name = '{}'
-        ORDER BY id ASC
-        '''.format(sys.argv[4])
+    query = '''
+            SELECT * FROM states
+            WHERE name = '{}'
+            ORDER BY id ASC
+            '''.format(sys.argv[4])
 
-with MySQLdb.connect(**credentials) as connection:
-    with connection.cursor() as cursor:
-        cursor.execute(query)
-        data_rows = cursor.fetchall()
+    with MySQLdb.connect(**credentials) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            data_rows = cursor.fetchall()
 
-print(*data_rows, sep='\n')
+    print(*data_rows, sep='\n')
+
+
+if __name__ == "__main__":
+    # don´t run when imported using class/def
+    # source link: t.ly/4Tx3
+    main()
