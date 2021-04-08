0ace3c4415ddac6aa5784d34e37b6bcec1c26a09
Alejandro Franco Cedeno <afrancocedeno@gmail.com>
2021-04-08 23:44:44 +0000

    02 | code try | script as an argument

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script sends a request, and displays the size of the body of the response
-curl -sI localhost:5000 | grep "Content-Length" | cut -d ' ' -f 2
+curl -sI $1 | grep "Content-Length" | cut -d ' ' -f 2
