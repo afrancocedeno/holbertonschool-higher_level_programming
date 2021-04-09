fd940fd95e6f9afc55dc986eb68841159a5caf62
Alejandro Franco Cedeno <afrancocedeno@gmail.com>
2021-04-09 20:33:19 +0000

    00 | file create | output done

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''module: header'''
+import requests
+import sys
+
+
+def main():
+    '''main: funciton that takes in a URL, sends a request to
+    the URL and displays the value of the variable X-Request-Id
+    '''
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    print(response.headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    main()
