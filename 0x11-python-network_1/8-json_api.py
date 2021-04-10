1b8371125badebdaafe35734e188707697760e2d
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-04-09 23:12:58 -0500

    00 | file create | output done

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''module: json api'''
+
+import requests
+import sys
+
+
+def main():
+    '''main: function that takes in a letter and sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter as a parameter.
+    '''
+
+    # check if no arguments
+    if len(sys.argv) == 0:
+        print('No result')
+
+    # if arguments
+    else:
+        url = 'http://0.0.0.0:5000/search_user'
+        letter = {'q': sys.argv[1]}
+        response = requests.post(url, letter)
+
+        try:
+            data = response.json()
+            print('[{}] {}'.format(data['id'], data['name']))
+        except ValueError:
+            print('Not a valid JSON')
+
+
+if __name__ == "__main__":
+    main()
