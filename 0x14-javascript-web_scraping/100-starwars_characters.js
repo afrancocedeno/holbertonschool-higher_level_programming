e0bb667d7b0ba4e112a70763fa086e6f6bc5a04b
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-05-05 11:35:45 -0500

    00 | file create | output done retrieving movies paths

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/* script that prints all the characters of a movie ID */
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const movieID = process.argv[2];
+
+request.get(url + movieID + '/', (error, _response, body) => {
+  if (error) { console.log(error); }
+
+  /* retrieve the path for each movie match */
+  const moviePaths = JSON.parse(body).characters;
+
+  for (const element in moviePaths) {
+    request.get(moviePaths[element], (error, _response, body) => {
+      if (error) { console.log(error); }
+      movieName = JSON.parse(body).name;
+      console.log(movieName);
+    });
+  }
+});
