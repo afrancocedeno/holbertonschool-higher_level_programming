89ca6192626458d61e3a1e151dfe06da85fb8376
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-05-04 23:48:47 -0500

    01 | code try | underscore prefix response for unused

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const movieID = process.argv[2];
 /* url Star wars API */
 const url = 'https://swapi-api.hbtn.io/api/films/';
 
-request.get(url + movieID, (error, response, body) => {
+request.get(url + movieID, (error, _response, body) => {
   if (error) { return console.log('error:', error); }
   console.log(JSON.parse(body).title);
 });
